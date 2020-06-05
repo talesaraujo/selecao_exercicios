3c5da3e1beea09ac9a2292d2503b1d470b7b9166
Tales Araujo <talesaraujodossantos@gmail.com>
2020-06-05 18:57:38 -0300

    Minor changes

--- solutions/ex3/cnn.py
+++ solutions/ex3/cnn.py
@@ -5,12 +5,12 @@ from keras.optimizers import SGD
 
 def create_model():
 	model = Sequential()
- 
-    # Convolutional stack
+
+    # 3-tier Convolutional stack
 	model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform', padding='same', input_shape=(200, 200, 3)))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(rate=0.2))
- 
+
 	model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(0.2))
@@ -18,7 +18,7 @@ def create_model():
 	model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', kernel_initializer='he_uniform', padding='same'))
 	model.add(MaxPooling2D(pool_size=(2, 2)))
 	model.add(Dropout(rate=0.2))
- 
+
     # Fully-connected stage
 	model.add(Flatten())
 	model.add(Dense(units=128, activation='relu', kernel_initializer='he_uniform'))
@@ -28,5 +28,5 @@ def create_model():
     # Model compiling
 	opt = SGD(lr=0.001, momentum=0.9)
 	model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
- 
+
 	return model
